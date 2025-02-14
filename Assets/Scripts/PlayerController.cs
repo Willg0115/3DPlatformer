370722dc6db99254a1587db90348a04c33c02c96
Willg0115 <willg0115@outlook.com>
2025-02-14 12:49:37 -0500

    added jumping and gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    [Header("Movement Settings")]
     public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public float gravity = -9.81f;//not using rigid body 
     public Transform cameraTransform;
 
+    [Header("Jump Settings")]
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public float groundDistance = 0.3f;
     
     private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private int jumpCount = 0;
     
-   
-
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -19,7 +26,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         HandleMovement();
+        HandleJump();
+        ApplyGravity();
     }
+
 
     void HandleMovement()
     {
@@ -32,8 +42,40 @@ public class PlayerController : MonoBehaviour
 
         moveDirection.y = 0; 
         controller.Move(moveSpeed * Time.deltaTime * moveDirection.normalized);
-    }   
+    }  
 
+    void HandleJump()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        if (isGrounded)
+        {
+            jumpCount = 0; 
+        }
+
+        if (Input.GetKey(KeyCode.Space) && jumpCount < 1)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+            jumpCount++;
+        }
+    }
+
+    void ApplyGravity()
+    {
+        if (!isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        else if (velocity.y < 0)
+        {
+            velocity.y = -2f; 
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+    }
 }
+ 
+
+
+
 
